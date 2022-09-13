bae3d5d2b2f72aeafe3e8f77da42fa5781d2596c
Yuliya Sidarenka <yulka1102@gmail.com>
2022-09-14 00:45:11 +0200

    hoc for list item page, check workability

--- src/components/ui/listitem/index.tsx
+++ src/components/ui/listitem/index.tsx
@@ -1,23 +1,26 @@
 import React, {ReactNode} from 'react';
 import {Item} from "../../../core/types";
 import "../../../styles/listitem/index.scss"
+import {Link} from "react-router-dom";
 
 const ListItem = ({ id, thumbnailUrl, title, children }: ListItemProps) => {
     return (
-        <div key={id} className='list-item'>
-            <div>
-                {children}
+        <Link to={`/${id}`}>
+            <div key={id} className='list-item'>
+                <div>
+                    {children}
+                </div>
+                <div className='list-item__image'>
+                    <img src={thumbnailUrl} alt="item"/>
+                </div>
+                <h3 className="list-item__title">{title}</h3>
             </div>
-            <div className='list-item__image'>
-                <img src={thumbnailUrl} alt="item"/>
-            </div>
-            <h3 className="list-item__title">{title}</h3>
-        </div>
+        </Link>
     );
 }
 
 export default ListItem;
 
 interface ListItemProps extends Item{
-    children: ReactNode,
+    children?: ReactNode,
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/homepage";
 import {Item} from "../core/types";
-import CardFromRoute from "../hocs/CardFromRoute";
+import PageFromRoute from "../hocs/PageFromRoute";
 
 const RouterComponent = ({ list }: RouterComponentProps) => {
     const routesAmount = list.length;
-    const routes = Array(routesAmount).fill('/').map((elem, index) => `${elem}${index+1}`);
+    const routes = Array(routesAmount).fill('').map((elem, index) => index + 1);
 
-    console.log(routes)
     return (
             <Routes>
                 <Route path="/" element={<Homepage list={list}/>}/>
                 {routes.map(route =>
-                    <Route path={route} element={<CardFromRoute route={route}/>}/>
+                    <Route key={route} path={'/' + route} element={<PageFromRoute route={route} shownListItem={list[route]}/>}/>
                 )}
             </Routes>
     )
